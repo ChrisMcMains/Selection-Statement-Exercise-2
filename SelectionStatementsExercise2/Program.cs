9d3b731f3f4580f6cf0eb4b40395f647da3148f2
chris mcmains <mcmainsc84@gmail.com>
2024-04-15 10:36:53 -0400

    selection statements 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject?");
+            
+            string subject = Console.ReadLine();
+            switch (subject.ToLower()) 
+            {
+                case "math":
+                    Console.WriteLine("Math is fun");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is magical");
+                    break;
+                case "gym":
+                case "pe":
+                    Console.WriteLine("Gym is my favorite");
+                    break;
+                case "history":
+                    Console.WriteLine("History is boring");
+                    break;
+                case "english":
+                    Console.WriteLine("English is needed in America");
+                    break;
+                default:
+                    Console.WriteLine("I don't know that subject");
+                    break;
+
+            }
         }
     }
 }
